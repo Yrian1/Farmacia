f372da29c16c9a30b950a2eb0b1ab10c6ed34807
Gabriel_Andrade <51178891+natsukiller12@users.noreply.github.com>
2022-05-02 23:24:01 -0300

    Update farmacia.java

--- farmacia.java
+++ farmacia.java
@@ -1,4 +1,5 @@
-package farmacia;
+package Farmacia;
+
 import java.util.ArrayList;
 
 public class Farmacia {
@@ -13,14 +14,19 @@ public class Farmacia {
 	 }
 	public static void main(String[] args) {
 		Farmacia farma= new Farmacia();
-		MedComum p1 = new MedComum(1, 2.60, 40);
-		MedNControlado p2 = new MedNControlado(2, 2.5, 30);
+		MedComum p1 = new MedComum(1, 2.60, 40);// inicia os medicamentos comuns adicionando um medicamento,seu preço e a quantidade dele
+		MedNControlado p2 = new MedNControlado(2, 2.5, 30);//inicia os medicamentos nao controlados adicionando um medicamento,seu preço e a quantidade dele
+		MedControlado p3= new MedControlado(4,2.4, 50);//inicia os medicamentos controlados adicionando um medicamento,seu preço, a quantidade dele
 		farma.addMed(p1);
 		farma.addMed(p2);
+		farma.addMed(p3);
 		p1.setQtdAtual(-10);
+		p2.setQtdAtual(26);
+		p3.devolver(5);
 
 		 System.out.println(p1.toString());
 		 System.out.println(p2.toString());
+		 System.out.println(p3.toString());
 		// TODO Auto-generated method stub
 
 	}
